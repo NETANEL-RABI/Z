02ffe08f0f6fba15aa80e6fcc51b4b7871143323
NETANEL-RABI <n0527648116@gmail.com>
2026-06-21 23:28:22 +0300

    Add Video class with properties and constructor

---
+++ app/src/main/java/com/ytpremium/app/Video.java
@@ -0,0 +1,13 @@
+package com.ytpremium.app;
+
+public class Video {
+    public String videoId;
+    public String title;
+    public String thumbnailUrl;
+
+    public Video(String videoId, String title, String thumbnailUrl) {
+        this.videoId = videoId;
+        this.title = title;
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
